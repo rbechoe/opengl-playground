7a50724665f919ccaad2da5a8f1b437d8ed2e746
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-05-12 18:26:56 +0200

    moved shader logic to seperate files and added texture loading

--- project folder/OpenGL/OpenGL/OpenGL.cpp
+++ project folder/OpenGL/OpenGL/OpenGL.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <glad\glad.h>
 #include "GLFW\glfw3.h"
+#include <shaders/shader.h> // is in include directory
+#include "stb_image.h"
 
 int main()
 {
@@ -22,21 +24,23 @@ int main()
 
     glViewport(0, 0, 640, 480);
 
+    Shader basicShader("shader.vs", "shader.fs");
+
     // START SETUP OBJECT
     // vertices of a quad
     float vertices[] =
     {
-        // position             // color                    // uv
-        -0.5f, -0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
-        0.5f, -0.5f, 0.5f,      0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f,      0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
-        0.5f, 0.5f, 0.5f,       1.0f, 0.0f, 1.0f, 1.0f,     1.0f, 1.0f,
+        // position             // color              // uv
+         0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,     1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,     1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,     0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 1.0f,     0.0f, 1.0f, // top left
     };
     // indices of quad
     unsigned int indices[] =
     {
         0, 1, 2,
-        1, 3, 2,
+        3, 0, 2,
     };
 
     unsigned int VAO; // vertex array object
@@ -53,77 +57,63 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 9, 0);
+    // position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (void*)(sizeof(float) * 3));
+    // color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (void*)(sizeof(float) * 7));
+    // uv
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
     glEnableVertexAttribArray(2);
 
     // clean up the buffer
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    // texture
+    unsigned int texture1, texture2;
+
+    // set texture 1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
+    // set texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
+    // apply texture
+    basicShader.use();
+    glUniform1i(glGetUniformLocation(basicShader.ID, "texture1"), 0);
+    basicShader.setInt("texture2", 1);
+
     // END SETUP OBJECT
-
-
-
-    // SETUP SHADER PROGRAM
-    const char* vertexSource = "#version 330 core\n"
-        "layout(location=0) in vec3 vPos;\n"
-        "layout(location=1) in vec4 vColor;\n"
-        "layout(location=2) in vec2 vUV;\n"
-        "out vec4 color;\n"
-        "out vec2 uv;\n"
-        "void main() {\n"
-        "\tgl_Position = vec4(vPos, 1.0);\n"
-        "\tcolor = vColor;\n"
-        "\tuv = vUV;\n"
-        "}\n"
-        "\0";
-
-    const char* fragmentSource = "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "in vec4 color;\n"
-        "in vec2 uv;\n"
-        "void main() {\n"
-        "\tFragColor = vec4(uv, 0.0, 1.0);\n"
-        "}\n"
-        "\0";
-
-    unsigned int vertID, fragID;
-    vertID = glCreateShader(GL_VERTEX_SHADER);
-    fragID = glCreateShader(GL_FRAGMENT_SHADER);
-
-    glShaderSource(vertID, 1, &vertexSource, nullptr);
-    glShaderSource(fragID, 1, &fragmentSource, nullptr);
-
-    int success;
-    char infoLog[512];
-
-    glCompileShader(vertID);
-    glGetShaderiv(vertID, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    };
-
-    glCompileShader(fragID);
-    glGetShaderiv(fragID, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    };
-
-    unsigned int myProgram = glCreateProgram();
-    glAttachShader(myProgram, vertID);
-    glAttachShader(myProgram, fragID);
-    glLinkProgram(myProgram);
-
-    glDeleteShader(vertID);
-    glDeleteShader(fragID);
-    // END SHADER PROGRAM
 
     while (!glfwWindowShouldClose(window))
     {
@@ -131,17 +121,26 @@ int main()
         float sinGreen = (float)sin(glfwGetTime() * 1.3f);
         float sinBlue = (float)sin(glfwGetTime() * 1.7f);
 
-        glUseProgram(myProgram);
-
         glClearColor(sinRed, sinGreen, sinBlue, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        basicShader.use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // memory management
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
